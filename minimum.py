a2c35bcae27bead44389091866926699cc79d0d2
rampreetha <53893791+rampreetha@users.noreply.github.com>
2019-08-08 14:34:49 +0530

    Create minimum.py

---
+++ minimum.py
@@ -0,0 +1,13 @@
+def maxminposition(A, n):
+  
+   minposition = A.index(min(A))
+   print ("The maximum is at position::", maxpos
+   maxposition = A.index(max(A)) ition + 1) 
+   print ("The minimum is at position::", minposition + 1)
+A=list()
+n=int(input("Enter the size of the List ::"))
+print("Enter the Element ::")
+for i in range(int(n)):
+   k=int(input(""))
+   A.append(k)
+maxminposition(A,n)
