02620701d3cf21342750ada97c605cfed2786ba6
rampreetha <53893791+rampreetha@users.noreply.github.com>
2019-08-08 14:11:02 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+int main()  
+{  
+    int a = 2;  
+    int d = 1; 
+    int N = 5;  
+    cout << "The "<< N  
+         <<"th term of the series is : "
+         << Nth_of_AP(a,d,N); 
+  
+    return 0; 
+} 
